5bb443892665f50e884f4fa53fa69a52c60bc179
Tamal Banerjee <88030568+tbanerje2@users.noreply.github.com>
2025-04-28 23:39:41 +0530

    feat: file name change

--- tests/smart_pay.spec.js
+++ tests/smart_pay.spec.js
@@ -1,53 +1,30 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 const { test, expect, selectors } = require('@playwright/test');
+import LandingPage from '../pages/landingPage';
+import PaymentMode from '../pages/payment_mode';
+import ChargebeeUtlis from '../utils/chargebee-utils';
+import SuccessPage from '../pages/successPage';
 
-test.describe('Buy Subscription from The New Yorker', () => {
-  test.describe('Buy Digital + Print Offer', () => {
+let userEmail = 'sb-msvw58545680@mailinator.com';
+
+test.describe('Buy Digital + Print Offer', () => {
     test('Pay with Paypal', async ({ page }) => {
-      selectors.setTestIdAttribute('data-cy');
+        const landingPage = new LandingPage(page)
+        const paymentMode = new PaymentMode(page)
+        const successPage = new SuccessPage(page)
+        selectors.setTestIdAttribute('data-cy');
+        await landingPage.navigate(page);
+        test.slow();
 
-      await page.goto('https://stg.newyorker.com/v2/offers/tny-ppu-demo');
-      test.slow();
+        await paymentMode.selectPaypal(page);
+        await paymentMode.payPal(page);
+        await paymentMode.purchaseThroughPayPal(page, userEmail);
+        await successPage.verifySuccessPage(page);
 
-      await page
-        .locator('section')
-        .filter({ hasText: 'Unlimited Digital + Print Access' })
-        .getByRole('button')
-        .click();
-
-      await page.getByRole('button', { name: 'PAY WITH', exact: true }).click();
-      // await page
-      //   .getByRole('checkbox', { name: 'renewal-notice-consent' })
-      //   .click();
-      const paypalPopupPromise = page.waitForEvent('popup');
-      //test.setTimeout(120000);
-      await page
-        .frameLocator('iframe[name^="__zoid__paypal_buttons"]')
-        .getByLabel('Pay with PayPal')
-        .click();
-      const paypalPage = await paypalPopupPromise;
-      await paypalPage.waitForLoadState();
-      const emailField = await paypalPage.getByPlaceholder(
-        'Email or mobile number'
-      );
-      const passwordField = await paypalPage.getByPlaceholder('Password');
-      await page.waitForTimeout(15000);
-      await emailField.fill('Condetest2024tax06@yopmail.com');
-      await emailField.press('Enter');
-
-      await page.waitForTimeout(15000);
-      await paypalPage.getByPlaceholder('Password');
-      await passwordField.fill('123@Conde');
-      await passwordField.press('Enter');
-      //await paypalPage.locator('[data-testid="submit-button-initial"]').click();
-      await expect(
-        page.getByText(
-          'You already have a subscription with this email address.'
-        )
-      ).toBeTruthy();
-
-      await paypalPage.getByRole('button', { name: 'Agree & Continue', exact: true }).click();
-      await page.getByRole('button', { name: 'No thanks, continue', exact: true }).click(); //No thanks, continue
     });
-  });
+
+    test.afterAll('CharegbeeDelete', async ({}) => {
+        const chargebeeUtlis = new ChargebeeUtlis()
+        await chargebeeUtlis.deleteChargebeeCustomerData(userEmail)
+    })
+
 });
