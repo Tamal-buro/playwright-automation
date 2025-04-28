93f9701cf9d4d2ef66a2b92045d796ab0c089a5e
Tamal Banerjee <88030568+tbanerje2@users.noreply.github.com>
2025-04-28 23:28:20 +0530

    feat: updated secrets

--- tests/smaprt_pay2.spec.js
+++ tests/smaprt_pay2.spec.js
@@ -10,7 +10,6 @@ test.describe('Buy Digital + Print Offer', () => {
     test('Pay with Paypal', async ({ page }) => {
         const landingPage = new LandingPage(page)
         const paymentMode = new PaymentMode(page)
-        const chargebeeUtlis = new ChargebeeUtlis()
         const successPage = new SuccessPage(page)
         selectors.setTestIdAttribute('data-cy');
         await landingPage.navigate(page);
@@ -20,43 +19,12 @@ test.describe('Buy Digital + Print Offer', () => {
         await paymentMode.payPal(page);
         await paymentMode.purchaseThroughPayPal(page, userEmail);
         await successPage.verifySuccessPage(page);
-        //await chargebeeUtlis.cleanChargebeeTestDtata(userEmail, request)
 
     });
 
-    test.afterAll('CharegbeeDelete', async ({ request }) => {
-        const pass = '';
-        const authHeader = `Basic ${Buffer.from(`${'test_H8qd4n9r60iiNB5kjDItE2LymaHjYH7Q'}:${pass}`).toString(
-            'base64'
-        )}`;
-
-        debugger
-        const getUrl = `https://condenast-staging-test.chargebee.com/api/v2/customers?email[is]=${userEmail}&limit=1`;
-        const getResponse = await request.get(getUrl, {
-            headers: {
-                Authorization: authHeader,
-            },
-        });
-        // expect(getResponse.status).toBe(200);
-        const data = await getResponse.json();
-        console.log('Tamal Response: ', getResponse)
-
-        if (data.list.length > 0) {
-            const customerId = data.list[0].customer.id;
-            console.log(`Customer ID: ${customerId}`);
-
-            const deleteUrl = `https://condenast-staging-test.chargebee.com/api/v2/customers/${customerId}/delete`;
-
-            const deleteResponse = await fetch(deleteUrl, {
-                method: 'POST',
-                headers: {
-                    Authorization: authHeader,
-                },
-            });
-            expect(deleteResponse.status).toBe(200)
-        } else {
-            console.log(`No customer found with email: ${userEmail}`);
-        }
+    test.afterAll('CharegbeeDelete', async ({}) => {
+        const chargebeeUtlis = new ChargebeeUtlis()
+        await chargebeeUtlis.deleteChargebeeCustomerData(userEmail)
     })
 
 });
