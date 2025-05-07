79c53d4a3ca9780024da2bcce739046b85a1f7bb
Tamal Banerjee <88030568+tbanerje2@users.noreply.github.com>
2025-05-07 16:28:34 +0530

    feat: update utils

--- utils/chargebee-utils.js
+++ utils/chargebee-utils.js
@@ -7,7 +7,7 @@ const chargebeeBaseUrl = () => {
 const pass = '';
 const chargebeeToken = process.env.CHARGEBEE_SECRET_KEY_STAG;
 
-const authHeader = `Basic ${Buffer.from(`test_H8qd4n9r60iiNB5kjDItE2LymaHjYH7Q:${pass}`).toString(
+const authHeader = `Basic ${Buffer.from(`${chargebeeToken}:${pass}`).toString(
     'base64'
 )}`;
 
